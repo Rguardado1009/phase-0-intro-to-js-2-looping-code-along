af7de6e1a3a9db246d93fcc66b063a68f14b539e
Rguardado1009 <rguardado1009@gmail.com>
2021-07-10 01:19:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const cards=[];
+function writeCards(array){
+    for (let i = 0; i < array.length; i++) {
+        cards.message = `Thank you, ${array[i]}, for the wonderful surprise gift!`
+        cards.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return cards;
+}
+writeCards(cards);
+  
+function countDown(integer){
+    
+   while (integer >=0) {
+       console.log(integer);
+       integer--;
+    
+    }
+  }
+  countDown(4);
